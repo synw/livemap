2295d2f1070810d7811239776b4bc1bb3c51fd7f
Win <synwe@yahoo.com>
2019-02-13 17:23:21 +0000

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geopoint/geopoint.dart';
 import 'package:livemap/livemap.dart';
 import 'package:latlong/latlong.dart';
 
-class LiveMapPage extends StatelessWidget {
+class LiveMapPage extends StatefulWidget {
+  @override
+  _LiveMapPageState createState() => _LiveMapPageState();
+}
+
+class _LiveMapPageState extends State<LiveMapPage> {
   static final MapController mapController = MapController();
   static final LiveMapController liveMapController =
       LiveMapController(mapController: mapController);
+  static final Stream<Position> positionStream =
+      PositionStream(timeInterval: 3).stream;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: LiveMap(
+          positionStream: positionStream,
           mapController: mapController,
           liveMapController: liveMapController,
           mapOptions: MapOptions(
@@ -32,6 +42,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mapjoe Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -40,5 +51,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+/*
+class LiveMapPage extends StatelessWidget {
+  
+}*/
 
 void main() => runApp(MyApp());
