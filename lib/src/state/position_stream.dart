4b4293d4872c8c2dbc78f70fa7c81e47e1328203
Win <synwe@yahoo.com>
2019-02-16 07:52:00 +0000

    Fix positionStream.enabled

--- lib/src/state/position_stream.dart
+++ lib/src/state/position_stream.dart
@@ -6,17 +6,14 @@ class PositionStreamState {
   PositionStreamState({@required this.changeFeedController})
       : assert(changeFeedController != null);
 
-  bool _enabled = true;
+  bool enabled = true;
   StreamController<LiveMapControllerStateChange> changeFeedController;
 
-  get enabled => _enabled;
-  set enabled(e) => _enabled = e;
-
   toggle() {
-    _enabled = !_enabled;
-    print("TOGGLE POSITION STREAM TO $_enabled");
+    enabled = !enabled;
+    print("TOGGLE POSITION STREAM TO $enabled");
     LiveMapControllerStateChange cmd =
-        LiveMapControllerStateChange(name: "positionStream", value: _enabled);
+        LiveMapControllerStateChange(name: "positionStream", value: enabled);
     changeFeedController.sink.add(cmd);
   }
 }
