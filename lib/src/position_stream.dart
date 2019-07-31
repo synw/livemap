92f313f11efe6b5318521b81b505f8221fcef93d
Win <synwe@yahoo.com>
2019-07-31 07:00:53 +0000

    Fix in position stream

--- lib/src/position_stream.dart
+++ lib/src/position_stream.dart
@@ -3,11 +3,16 @@ import 'package:geolocator/geolocator.dart';
 /// Initialize the position stream
 Stream<Position> initPositionStream(
     {int timeInterval = 3000, int distanceFilter}) {
-  Stream<Position> positionStream = Geolocator()
-      .getPositionStream(LocationOptions(
-          accuracy: LocationAccuracy.best,
-          timeInterval: timeInterval,
-          distanceFilter: distanceFilter))
-      .asBroadcastStream();
+  var geolocator = Geolocator();
+  LocationOptions options;
+  if (distanceFilter == null) {
+    options = LocationOptions(
+        accuracy: LocationAccuracy.best, timeInterval: timeInterval);
+  } else {
+    options = LocationOptions(
+        accuracy: LocationAccuracy.best, distanceFilter: distanceFilter);
+  }
+  Stream<Position> positionStream =
+      geolocator.getPositionStream(options).asBroadcastStream();
   return positionStream;
 }
