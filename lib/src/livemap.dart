c0724ffa185a8488cf3bd235348582060c5456c8
synw <synw@yahoo.com>
2020-02-26 16:43:44 +0100

    Fix for extra layers

--- lib/src/livemap.dart
+++ lib/src/livemap.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:fluxmap/fluxmap.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
 import 'controller.dart';
 
 class _LiveMapState extends State<LiveMap> {
-  _LiveMapState({@required this.controller, this.center, this.zoom = 2.0})
+  _LiveMapState(
+      {@required this.controller,
+      this.center,
+      this.zoom = 2.0,
+      this.extraLayers = const <LayerOptions>[]})
       : assert(controller != null) {
     center ??= LatLng(0.0, 0.0);
   }
@@ -13,23 +18,28 @@ class _LiveMapState extends State<LiveMap> {
   LiveMapController controller;
   LatLng center;
   final double zoom;
+  final List<LayerOptions> extraLayers;
 
   @override
   Widget build(BuildContext context) {
     return FluxMap(
-      state: controller.flux,
-      devicesFlux: controller.devicesFlux.stream,
-      networkStatusLoop: false,
-      center: center,
-      zoom: zoom,
-    );
+        state: controller.flux,
+        devicesFlux: controller.devicesFlux.stream,
+        networkStatusLoop: false,
+        center: center,
+        zoom: zoom,
+        extraLayers: extraLayers);
   }
 }
 
 /// The main livemap class
 class LiveMap extends StatefulWidget {
   /// Provide a controller
-  const LiveMap({@required this.controller, this.center, this.zoom = 2.0});
+  const LiveMap(
+      {@required this.controller,
+      this.center,
+      this.zoom = 2.0,
+      this.extraLayers = const <LayerOptions>[]});
 
   /// The map controller
   final LiveMapController controller;
@@ -39,6 +49,9 @@ class LiveMap extends StatefulWidget {
 
   /// The default zoom
   final double zoom;
+
+  /// Extra map layers
+  final List<LayerOptions> extraLayers;
 
   @override
   _LiveMapState createState() =>
